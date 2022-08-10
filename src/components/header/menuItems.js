87521d04fc780986126f2112dd94834b7f455c9a
santiago guzman arango <santiagoguzmanarango@MacBook-Pro-de-santiago.local>
2022-08-10 08:10:35 -0500

    ITG-03

---
+++ src/components/header/menuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    Title: 'PUFI PUFF',
+    url: '#',
+    cName: 'navLinks'
+  },
+  {
+    Title: 'PUFI RAIN',
+    url: '#',
+    cName: 'navLinks'
+  },
+  {
+    Title: 'PUFI CART',
+    url: '#',
+    cName: 'navLinks'
+  },
+  {
+    Title: 'PUFI NAP',
+    url: '#',
+    cName: 'navLinks'
+  }
+];
